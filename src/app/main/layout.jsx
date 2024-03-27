42e37d3d6231fa7e2e65da19c68c769b71687965
viswhwakarma-hemant <hemantk3335@gmai.com>
2024-03-27 12:51:27 +0530

    new updates

---
+++ src/app/main/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './navbar'
+
+const MainLayout = ({children}) => {
+  return (
+    <>
+    <Navbar />
+    {children}</>
+  )
+}
+
+export default MainLayout
---
+++ src/app/main/login/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      <h1>Hemant Vishwakarma</h1>
+    </div>
+  )
+}
+
+export default login;
