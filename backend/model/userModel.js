1a9133655bccde800f5093e61fa978a20bfebe8d
viswhwakarma-hemant <hemantk3335@gmai.com>
2024-05-16 11:32:09 +0530

    change user profile

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,4 +1,4 @@
-const {Schema, model } = require('../connection')
+const {Schema, model } = require('../connection');
 const mySchema = new Schema ({
     name : {type: String, required : true},
     email :{type:String, required:true, unique:true},
--- frontend/src/app/user/profile/page.jsx
+++ frontend/src/app/user/profile/page.jsx
@@ -1,92 +1,99 @@
 'use client';
-import { Card, Avatar,ActionIcon,Box, Text, Group, Button,Container,Overlay } from '@mantine/core';
+import { Card, Avatar, Text, Container, Button ,TextInput,Box,ActionIcon, Dialog} from '@mantine/core';
 import classes from './userProfile.module.css';
-import Link from 'next/link';
 import {
   IconBrandGithub,
+  IconBrandGmail,
   IconBrandInstagram,
   IconBrandLinkedin,
   IconImageInPicture,
 } from "@tabler/icons-react";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useRef } from 'react';  
+import Link from 'next/link';
 
-const stats = [
-  { value: '34K', label: 'Followers' },
-  { value: '187', label: 'Follows' },
-  { value: '1.6K', label: 'Posts' },
-];
 
-const UserProfile =()=> {
-  const router = useRouter();
-  const [isHovered, setIsHovered] = useState(false);
+const Profile = ()=> {
 
-const [ currentUser,setCurrentUser] = useState(
+  const inputRef = useRef(null);  
+  const[image,setImage] = useState('')
+  const[imagecrop,setImagecrop] = useState('')  
+
+  const handleImageClick = () => {
+    console.log('image clicked')
+    inputRef.current.click();
+  }
+  const handleImageChange = (event) => {  
+     const file = event.target.files[0];
+     setImage(event.target.files[0])
+  }
+
+   const [ currentUser,setCurrentUser] = useState(
   JSON.parse(sessionStorage.getItem('user'))
 ) 
-const handleMouseEnter = () => {
-  setIsHovered(true);
-};
-
-const handleMouseLeave = () => {
-  setIsHovered(false);
-};
-
-const fetchUserData  = () => {
-  fetch(`http://localhost:5000/user/getUser`,)
-.then(res => {
-  console.log(res.status);
-  return res.json();
-})
-.then(data => {
-  console.log(data);
-  router.push('/user/profile/editProfile')
-  setCurrentUser(data)
-})
-.catch(err => {
-  console.log(err);
-});
-}
-
-useEffect(() => {
-  fetchUserData();
-},[])
-
-  const items = stats.map((stat) => (
-    <div key={stat.label}>
-      <Text ta="center" fz="lg" fw={500}>
-        {stat.value}
-      </Text>
-      <Text ta="center" fz="sm" c="dimmed" lh={1}>
-        {stat.label}
-      </Text>
-    </div>
-  ));
-
+  const fetchUserData  = () => {
+    fetch(`http://localhost:5000/user/getUser`,)
+  .then(res => {
+    console.log(res.status);
+    return res.json();
+  })
+  .then(data => {
+    console.log(data);
+    router.push('/user/profile/editProfile')
+    setCurrentUser(data)
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  }
+  useEffect(() => {
+    fetchUserData();
+  },[])
+  
   return (
-  <Box className={classes.boxes}>
-    <Container fluid className={classes.wrapper}>
-    <Card className={classes.card}>
-      <Overlay className={classes.overlay} opacity={0.55} zIndex={0} />
-
-      <div className={classes.content}>
-        <Text size="lg" className={classes.title} mt={"xl"}>
-          
-          {currentUser.name}
-        </Text>
-
-        <Text size="xs" className={classes.description}>
-          Web Developer
-        </Text>
-        <Box className={classes.btn} mt={"xl"}>
-          <ActionIcon 
-            className={classes.pip}
-            component={Link}
-            href="https://github.com/vishwakrma-hemant?tab=repositories"
-          >
-            <IconImageInPicture className={classes.place} />
-          </ActionIcon>
-          <ActionIcon
+    <Card withBorder padding="xl" radius="md" className={classes.card}>
+      <Card.Section
+        h={140}
+        style={{
+          backgroundImage:
+            'url(https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80)',
+        }}
+      />
+      <Container fluid className={classes.box_btn}>
+      <Box onClick={handleImageClick}>
+       {image ? <img src={URL.createObjectURL(image)} alt="profile" style={{ width:'120px', height:'120px',
+        borderRadius:'50%', objectFit:'cover'
+       }} /> :<Avatar
+        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-9.png"
+        size={80}
+        radius={80}
+        mx="auto"
+        mt={-30}
+        className={classes.avatar}
+      />}
+      <Dialog
+      visibleFrom={imagecrop}
+      header={()=>(
+        <Text htmlfor='' className='text-2xl font-semibold textColor'>updateprofile</Text>)}
+        onHide={()=>setImagecrop(false)}
+      >
+        <Text>hi</Text>
+      </Dialog>
+      
+      <TextInput type="file"
+         ref={inputRef}
+           accept='image/*'
+           onChange={handleImageChange}
+           style={{display:'none'}}
+          />
+          </Box>
+      <Text ta="center" fz="lg" fw={500} mt="sm">
+        {currentUser.name}
+      </Text>
+      <Text ta="center" fz="sm" c="dimmed">
+        Developer
+      </Text>
+      <Box className={classes.btn} mt={"xl"}>
+      <ActionIcon
             // color="blue"
             radius="xl"
             size="xl"
@@ -122,13 +129,11 @@ useEffect(() => {
             component={Link}
             href="https://hemantk3335@gmail.com"
           >
-            <IconBrandInstagram size={33} className={classes.btn_icon} />
+            <IconBrandGmail size={33} className={classes.btn_icon} />
           </ActionIcon>
-        </Box>
-      </div>
+      </Box>
+          </Container>
     </Card>
-</Container>
-</Box>
   );
 }
-export default UserProfile;
+export default Profile;
