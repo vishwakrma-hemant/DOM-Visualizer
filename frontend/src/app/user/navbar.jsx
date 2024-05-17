a62925b7515ab67db9138110be7f26002d76793c
viswhwakarma-hemant <hemantk3335@gmai.com>
2024-05-17 20:23:21 +0530

    change

--- frontend/src/app/user/navbar.jsx
+++ frontend/src/app/user/navbar.jsx
@@ -33,6 +33,7 @@ import Link from 'next/link';
 const user = {
   name: '',
   email: '',
+  image: 'https://img.freepik.com/premium-vector/businessman-character-avatar-isolated_24877-5037.jpg?w=740',
 };
 
 const Navbar = () => {
@@ -62,7 +63,8 @@ const Navbar = () => {
                 className={cx(classes.user, { [classes.userActive]: userMenuOpened })}
               >
                 <Group gap={7}>
-                  <Avatar src={'http://localhost:5000/'+currentUser.avatar} alt={user.name} radius="xl" size={20} />
+                <Avatar src={user.image} alt={user.name} radius="xl" size={20} />
+                  {/* <Avatar src={'http://localhost:5000/'+currentUser.avatar} alt={user.name} radius="xl" size={20} /> */}
                   <Text fw={500} size="sm" lh={1} mr={3}>
                     {currentUser && currentUser.name}
                   </Text>
